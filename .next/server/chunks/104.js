2f663aabfcc077381bf7ba441a8caac669ace50e
Vincenttnecniv <99797375+Vincenttnecniv@users.noreply.github.com>
2022-08-09 12:54:50 +0200

    try build

---
+++ .next/server/chunks/104.js
@@ -0,0 +1,77 @@
+"use strict";
+exports.id = 104;
+exports.ids = [104];
+exports.modules = {
+
+/***/ 104:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_dist_shared_lib_styled_jsx__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(561);
+/* harmony import */ var next_dist_shared_lib_styled_jsx__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_dist_shared_lib_styled_jsx__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_Link__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(514);
+/* harmony import */ var next_Link__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_Link__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+const Nav = ()=>{
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: [
+            react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_dist_shared_lib_styled_jsx__WEBPACK_IMPORTED_MODULE_1___default()), {
+                id: "31bbe4870bdf0cad",
+                children: "html{width:100%;height:100%;background-color:#28282b}"
+            }),
+            react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_dist_shared_lib_styled_jsx__WEBPACK_IMPORTED_MODULE_1___default()), {
+                id: "d900c4a9b06fe6d",
+                children: "nav.jsx-5ae27cbf9f8b356d{background-color:orange;width:40%;text-align:center;-webkit-border-radius:25px;-moz-border-radius:25px;border-radius:25px;height:70px;margin-left:480px;padding:5px;padding-bottom:0px}a.jsx-5ae27cbf9f8b356d:hover{background-color:#28282b;color:lightGray}a.jsx-5ae27cbf9f8b356d{display:inline-block;padding:15px;font-size:25px;font-family:Garamond;color:#28282b;border:6px solid orange;-webkit-border-radius:36px;-moz-border-radius:36px;border-radius:36px;text-decoration:none;height:30%}nav.jsx-5ae27cbf9f8b356d:hover{outline-offset:-5px;outline:4px solid lightGray}"
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("nav", {
+                className: "jsx-5ae27cbf9f8b356d",
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_Link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                        href: "/",
+                        /*#__PURE__*/ children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                            className: "jsx-5ae27cbf9f8b356d",
+                            children: "Home"
+                        })
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_Link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                        href: "/about",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                            className: "jsx-5ae27cbf9f8b356d",
+                            children: "About"
+                        })
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_Link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                        href: "/contact",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                            className: "jsx-5ae27cbf9f8b356d",
+                            children: "Contact"
+                        })
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_Link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                        href: "/projects",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                            className: "jsx-5ae27cbf9f8b356d",
+                            children: "Projects"
+                        })
+                    })
+                ]
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("br", {
+                className: "jsx-5ae27cbf9f8b356d"
+            })
+        ]
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Nav);
+
+
+/***/ })
+
+};
+;
